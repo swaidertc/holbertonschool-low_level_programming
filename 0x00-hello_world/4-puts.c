6cfc8344e37c018808aa3ed8888fcf293abcbbec
swaidertc <swaikox@gmail.com>
2022-02-10 15:03:56 -0800

    P

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,4 @@
 #include <stdio.h>
-<<<<<<< HEAD
-
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-=======
 /**
 * main - blok 
 * Return: 0
@@ -14,5 +7,4 @@ int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
->>>>>>> 623c93766d31930cbb7622c17f1d42ae19d77b9e
 }
