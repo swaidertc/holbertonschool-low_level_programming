55d0b81f9dbd5b4b485232aa64582cdb94fb6e7e
swaidertc <54114754+swaidertc@users.noreply.github.com>
2022-02-10 14:07:07 -0500

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
-/**      
+/**
+ * Main - Prints "programming is like building a multilingual.
+ *                
+ *
  * Return: Always 0.
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
