40f30b9613eb80fb2380cc5a49554af9968d5d19
swaidertc <54114754+swaidertc@users.noreply.github.com>
2022-02-10 17:19:59 -0500

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - Prints "Write"
+*                
+* Return: 1
+*/
+int main(void)
+{
+write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+return (1);
+}
