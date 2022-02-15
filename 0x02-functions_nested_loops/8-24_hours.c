9d98cabcfa534ba3033f9865c38b1ae3d63c90c3
swaidertc <swaikox@gmail.com>
2022-02-15 06:25:54 -0800

    e

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,3 +1,8 @@
+/*
+ * File: 8-24_hours.c
+ * Auth: Swaidertc
+ */
+
 #include "main.h"
 
 /**
@@ -6,18 +11,18 @@
  */
 void jack_bauer(void)
 {
-	int hour, minute;
+int hour, minute;
 
-	for (hour = 0; hour <= 23; hour++)
-	{
-		for (minute = 0; minute <= 59; minute++)
-		{
-			_putchar((hour / 10) + '0');
-			_putchar((hour % 10) + '0');
-			_putchar(':');
-			_putchar((minute / 10) + '0');
-			_putchar((minute % 10) + '0');
-			_putchar('\n');
-		}
-	}
+for (hour = 0; hour <= 23; hour++)
+{
+for (minute = 0; minute <= 59; minute++)
+{
+_putchar((hour / 10) + '0');
+_putchar((hour % 10) + '0');
+_putchar(':');
+_putchar((minute / 10) + '0');
+_putchar((minute % 10) + '0');
+_putchar('\n');
+}
+}
 }
