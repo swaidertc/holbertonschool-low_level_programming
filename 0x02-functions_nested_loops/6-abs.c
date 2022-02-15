8bed92df6115cd91b5d0842ed349bbf698f0401d
swaidertc <swaikox@gmail.com>
2022-02-15 05:25:02 -0800

    d

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,4 +1,10 @@
+/*
+ * File: 6-abs.c
+ * Auth: Swaidertc
+ */
+
 #include "main.h"
+
 /**
  * _abs - Computes the absolute value of an integer.
  * @n: The integer to be computed.
@@ -7,8 +13,8 @@
  */
 int _abs(int n)
 {
-	if (n >= 0)
-		return (n);
-	else
-		return (-n);
+if (n >= 0)
+return (n);
+else
+return (-n);
 }
