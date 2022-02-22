a96ea20635ba0c9401e0bde85c62d890a069392a
swaidertc <swaikox@gmail.com>
2022-02-21 17:06:02 -0800

    Puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+/*
+ * File: 3-puts.c
+ * Auth: Swaidertc
+ */
+
+#include "main.h"
+
+/**
+ * _puts - Prints a string to stdout.
+ * @str: The string to be printed.
+*/
+void _puts(char *str)
+{
+int i;
+
+for (i = 0; str[i] != '\0'; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
