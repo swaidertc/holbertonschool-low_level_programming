c4659cac86ff3ba16523d285f7fd4b0d5fd83749
swaidertc <swaikox@gmail.com>
2022-02-28 22:25:54 -0500

    Str

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,32 @@
+/*
+ * File: 2-strchr.c
+ * Auth: Swaidertc
+*/
+
+#include "main.h"
+
+/**
+ * _strchr - Locates a character in a string.
+ * @s: The string to be searched.
+ * @c: The character to be located.
+ *
+ * Return: If c is found - a pointer to the first occurence.
+ *         If c is not found - NULL.
+*/
+char *_strchr(char *s, char c)
+{
+int a;
+
+while (1)
+{
+a = *s++;
+if (a == c)
+{
+return (s - 1);
+}
+if (a == 0)
+{
+return (NULL);
+}
+}
+}
