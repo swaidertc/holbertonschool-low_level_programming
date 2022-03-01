5f44aec8ebee0ec5d7d007f7fb15fbb2232fba2d
swaidertc <swaikox@gmail.com>
2022-02-28 22:21:14 -0500

    Upda

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -16,12 +16,12 @@
 */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-unsigned int index;
-unsigned char *destination = dest;
-const unsigned char *source = src;
+	unsigned int i;
 
-for (index = 0; index < n; index++)
-destination[index] = source[index];
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
 
-return (dest);
+	return (dest);
 }
